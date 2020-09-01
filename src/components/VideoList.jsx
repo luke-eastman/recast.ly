75a08f80297fd8d2562d1bd708442838f3eba5d1
Luke Eastman <LukeAEastman@gmail.com>
2020-09-01 10:11:06 -0700

    refact videoList as stateless component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,19 +1,11 @@
 import App from './App.js';
 import VideoListEntry from './VideoListEntry.js';
 
-class VideoList extends App {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="video-list">
-        {this.props.videos.map(video => <VideoListEntry video={video}/>)}
-      </div>
-    );
-  }
-}
+var VideoList = (props) => (
+  <div className="video-list">
+    {props.videos.map(video => <VideoListEntry video={video}/>)}
+  </div>
+);
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
