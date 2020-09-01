6ceb2ea09f4558bd32940879d16462c6b1733681
Luke Eastman <LukeAEastman@gmail.com>
2020-08-31 19:59:50 -0700

    add video player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import exampleVideoData from '../data/exampleVideoData.js';
+import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
 var App = () => (
   <div>
@@ -9,7 +10,7 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><VideoPlayer video={exampleVideoData[0]} /></div>
       </div>
       <div className="col-md-5">
         <div><VideoList videos={exampleVideoData} /></div>
